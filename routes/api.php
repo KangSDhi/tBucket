c9e85b3ff1ac8c8c7f7e1c4b8531f1886cbc5abe
KangSDhi <kangteknisi@gmail.com>
2020-09-12 14:55:58 +0700

    test passport laravel

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\UserController;
 
 /*
 |--------------------------------------------------------------------------
-| API Routes
+| API Routess
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
@@ -14,6 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::post('loginAPI', [UserController::class, 'login']);
+Route::post('registerAPI', [UserController::class, 'register']);
+
+Route::group(['middleware' => 'auth:api'], function (){
+    Route::post('detailsAPI', [UserController::class, 'details']);
 });
